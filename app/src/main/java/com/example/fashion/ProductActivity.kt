6bf2eb0afdc07bf3d18bf640c5633e206ad1c0eb
LindaDavid123 <lindadavid@students.amikom.ac.id>
2025-01-07 17:14:17 +0700

    update produk

--- app/src/main/java/com/example/fashion/ProductActivity.kt
+++ app/src/main/java/com/example/fashion/ProductActivity.kt
@@ -1,20 +1,51 @@
 package com.example.fashion
 
 import android.os.Bundle
+import android.util.Log
+import android.view.View
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import androidx.recyclerview.widget.GridLayoutManager
+import androidx.recyclerview.widget.RecyclerView
+import com.example.fashion.client.RetrofitClient
+import com.example.fashion.response.produk.ProdukResponse
+import retrofit2.Call
+import retrofit2.Callback
+import retrofit2.Response
 
 class ProductActivity : AppCompatActivity() {
+    private  val listProduk = ArrayList<ProdukResponse>()
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContentView(R.layout.activity_product)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
-        }
+
+        val RVQuiz: RecyclerView = findViewById(R.id.recyclerViewProduk)
+        RVQuiz.layoutManager = GridLayoutManager(this@ProductActivity, 2)
+
+        RetrofitClient.instance.getProduk().enqueue(
+            object : Callback<ArrayList<ProdukResponse>> {
+                override fun onResponse(
+                    call: Call<ArrayList<ProdukResponse>>,
+                    response: Response<ArrayList<ProdukResponse>>
+                ) {
+                    listProduk.clear()
+                    response.body().let {
+                        if (it != null) {
+                            listProduk.addAll(it)
+                        }
+                    }
+                    var adapter = AdapterProduk(listProduk)
+                    RVQuiz.adapter = adapter
+                }
+
+                override fun onFailure(p0: Call<ArrayList<ProdukResponse>>, p1: Throwable) {
+                    Log.e("API Error", "Request failed: ${p1.message}")
+                }
+            }
+        )
     }
 }
