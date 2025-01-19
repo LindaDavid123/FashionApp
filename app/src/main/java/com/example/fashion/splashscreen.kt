abdec072816ccfff2b0eec2bdc27c18d96949b4b
meibeash <delphilaode@students.amikom.ac.id>
2025-01-19 19:58:44 +0700

    update home

---
+++ app/src/main/java/com/example/fashion/splashscreen.kt
@@ -0,0 +1,23 @@
+package com.example.fashion
+
+import android.content.Intent
+import android.os.Bundle
+import android.os.Handler
+import android.os.Looper
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+
+class splashscreen : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContentView(R.layout.activity_splashscreen)
+        Handler(Looper.getMainLooper()).postDelayed({
+            val intent = Intent(this@splashscreen, MainActivity::class.java)
+            startActivity(intent)
+            finish() // Mengakhiri SplashActivity agar tidak kembali dengan tombol Back
+        }, 3000) // 3000ms = 3 detik
+    }
+}
